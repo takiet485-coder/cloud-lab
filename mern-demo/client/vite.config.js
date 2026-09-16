734fa77718aebec0f3810427f52b0f692b0b065f
takiet485-coder <takiet485@gmail.com>
2026-09-16 03:39:09 +0000

    Complete Lab 04 Docker MERN

--- mern-demo/client/vite.config.js
+++ mern-demo/client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://backend:5000',
         changeOrigin: true,
         secure: false,
       }
